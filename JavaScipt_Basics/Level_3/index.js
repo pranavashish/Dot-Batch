d80be1fad00cf9b0d88cee4a622ea30506e1fea3
pranavashish <pranavashish@gmail.com>
2023-02-16 01:24:59 +0530

    Level_3_Completed

--- JavaScipt_Basics/Level_3/index.js
+++ JavaScipt_Basics/Level_3/index.js
@@ -9,6 +9,10 @@
 
 // console.log(lastname.charAt(2));
 
+// -----------------------------------------------------------------------------
+
+// @@@ BACKTICK
+
 // console.log(
 //   `Hello ${firstname},
 
@@ -25,13 +29,17 @@
 
 // console.log(date3);
 
-// arrays
+// -----------------------------------------------------------------------------
+
+// @@@ arrays
 
 // let numbers = [1, 2, 3, 4];
 
 // console.log(numbers);
 
-//ADDING ELEMENT
+// -----------------------------------------------------------------------------
+
+// @@@ ADDING ELEMENT
 
 // END       --> push
 // Beginning --> unshift
@@ -39,7 +47,9 @@
 
 // console.log(numbers.includes(30));
 
-// SEARCHING
+// -----------------------------------------------------------------------------
+
+// @@@ SEARCHING
 
 // Primitive --> indexOF, includes
 // Object/Refrences --> callback functions
@@ -59,24 +69,168 @@
 
 // console.log(course);
 
-//ARROW FUNCTIONS
+// -----------------------------------------------------------------------------
+// @@@ ARROW FUNCTIONS
 
 // let courseArrow = courses.find((course) => course.name === "Pranav");
 
 // console.log(courseArrow);
 
-//Removing element
+// -----------------------------------------------------------------------------
+
+// @@@ Removing element
 
 // END       --> pop
 // Beginning --> shift
 // Middle    --> splice(index, count(no of element u want to delete),)
 
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-numbers.pop();
+// numbers.pop();
 
-numbers.shift();
+// numbers.shift();
 
-numbers.splice(1, 3);
+// numbers.splice(1, 3);
 
-console.log(numbers);
+// console.log(numbers);
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let numbers2 = numbers;
+
+// numbers = [];
+
+// console.log(numbers);
+
+// console.log(numbers2);
+
+// -----------------------------------------------------------------------------
+
+// @@@ COMBINING & SLICING ARRAYS  -> concat() , slice()
+
+// let num1 = [1, 2, 3, 4, 5];
+// let num2 = [6, 7, 8, 9, 10];
+
+// let num3 = num1.concat(num2);
+
+// console.log(num3);
+
+// let num4 = num3.slice(0, 5);
+
+// console.log(num4);
+
+// FULL SLICING -->  slice()
+
+// -----------------------------------------------------------------------------
+
+// @ Spread Operator
+
+// let num1 = [1, 2, 3, 4, 5];
+// let num2 = [6, 7, 8, 9, 10];
+
+// let combined = [...num1, ...num2];
+
+// let combined = [...num1, "a", false, "b", ...num2];
+
+// console.log(combined);
+
+// -----------------------------------------------------------------------------
+
+// @ Iterating an ARRAY
+
+// for of
+
+// let arr = [10, 20, 30, 40, 50];
+
+// for (let value of arr) {
+//   console.log(value);
+// }
+
+// forEach
+
+// arr.forEach((number) => console.log(number));
+
+// -----------------------------------------------------------------------------
+
+// @ Joining ARRAY
+
+// let arr = [10, 20, 30, 40, 50];
+
+// const joined = arr.join(" ");
+
+// console.log(joined);
+
+// -----------------------------------------------------------------------------
+
+// @ Split ARRAY
+
+// let message = "This is my first message";
+// let parts = message.split(" ");
+
+// console.log(parts);
+
+// let joined = parts.join(" ");
+// console.log(joined);
+
+// -----------------------------------------------------------------------------
+
+// @ Sorting ARRAY
+
+// let arr = [40, 80, 10, 30, 20, 35, 50, 40];
+
+// let fin1 = arr.sort();
+
+// let fin = fin1.reverse();
+
+// console.log(fin);
+
+// let arr = [
+//   { id: 1, name: "Pranav" },
+//   { id: 2, name: "Ashish" },
+// ];
+
+// arr.sort((a1, a2) => a1.name > a2.name);
+// console.log(arr);
+
+// -----------------------------------------------------------------------------
+
+// @ filtering ARRAY .filter(callback)
+
+// let arr = [1, 2, -3, -5];
+
+// let filtered = arr.filter((value) => value >= 0);
+
+// console.log(filtered);
+
+// -----------------------------------------------------------------------------
+
+// @  Mapping ARRAY *(.map)  => Map each element of array to something else
+
+// let arr = [7, 8, 9, 10];
+
+// console.log(arr);
+
+// let items = arr.map((value) => "student_no " + value);
+
+// console.log(items);
+
+// -----------------------------------------------------------------------------
+
+// @  Mapping OBJECTS *(.map)
+
+let number = [1, 2, -6, -9];
+
+// let filtered = number.filter((value) => value >= 0);
+
+// let items = filtered.map(function (num) {
+//   return { value: num };
+// });
+
+//chaining
+
+let items = number
+  .filter((value) => value >= 0)
+  .map(function (num) {
+    return { value: num };
+  });
+
+console.log(items);
