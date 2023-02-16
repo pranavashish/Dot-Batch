35ac6850712d1e64e5fd07c06ebfb01bba0eda79
pranavashish <pranavashish@gmail.com>
2023-02-17 00:07:50 +0530

    JavaScipt_Basics Completed

--- JavaScipt_Basics/Level_3/index.js
+++ JavaScipt_Basics/Level_3/index.js
@@ -1,41 +1,41 @@
-// console.log("Level #3");
+console.log("Level #3");
 
-// let lastname = "Ashish";
+let lastname = "Ashish";
 
-// let firstname = new String("Pranav");
+let firstname = new String("Pranav");
 
-// console.log(typeof lastname);
-// console.log(typeof firstname);
+console.log(typeof lastname);
+console.log(typeof firstname);
 
-// console.log(lastname.charAt(2));
+console.log(lastname.charAt(2));
 
 // -----------------------------------------------------------------------------
 
 // @@@ BACKTICK
 
-// console.log(
-//   `Hello ${firstname},
+console.log(
+  `Hello ${firstname},
 
-// Thanks for learning MERN Stack
+Thanks for learning MERN Stack
 
-// Regards,
-// ${lastname}`
-// );
-// let date = new Date();
-// let date2 = new Date("Apr 01 2000 9:50");
-// let date3 = new Date(2000, 3, 01, 9);
+Regards,
+${lastname}`
+);
+let date = new Date();
+let date2 = new Date("Apr 01 2000 9:50");
+let date3 = new Date(2000, 3, 01, 9);
 
-// date3.setFullYear(2004);
+date3.setFullYear(2004);
 
-// console.log(date3);
+console.log(date3);
 
 // -----------------------------------------------------------------------------
 
 // @@@ arrays
 
-// let numbers = [1, 2, 3, 4];
+let numberz = [1, 2, 3, 4];
 
-// console.log(numbers);
+console.log(numberz);
 
 // -----------------------------------------------------------------------------
 
@@ -45,7 +45,7 @@
 // Beginning --> unshift
 // Middle    --> splice
 
-// console.log(numbers.includes(30));
+console.log(numbers.includes(30));
 
 // -----------------------------------------------------------------------------
 
@@ -54,27 +54,27 @@
 // Primitive --> indexOF, includes
 // Object/Refrences --> callback functions
 
-// let courses = [
-//   { no: 1, name: "Pranav" },
-//   { no: 2, name: "Ashish" },
-// ];
+let courses = [
+  { no: 1, name: "Pranav" },
+  { no: 2, name: "Ashish" },
+];
 
-// console.log(courses);
+console.log(courses);
 
-// console.log(courses.includes({ no: 1, name: "Pranav" }));
+console.log(courses.includes({ no: 1, name: "Pranav" }));
 
-// let course = courses.find(function (course) {
-//   return course.name == "Pranav";
-// });
+let course = courses.find(function (course) {
+  return course.name == "Pranav";
+});
 
-// console.log(course);
+console.log(course);
 
 // -----------------------------------------------------------------------------
 // @@@ ARROW FUNCTIONS
 
-// let courseArrow = courses.find((course) => course.name === "Pranav");
+let courseArrow = courses.find((course) => course.name === "Pranav");
 
-// console.log(courseArrow);
+console.log(courseArrow);
 
 // -----------------------------------------------------------------------------
 
@@ -84,39 +84,39 @@
 // Beginning --> shift
 // Middle    --> splice(index, count(no of element u want to delete),)
 
-// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// numbers.pop();
+numbers.pop();
 
-// numbers.shift();
+numbers.shift();
 
-// numbers.splice(1, 3);
+numbers.splice(1, 3);
 
-// console.log(numbers);
+console.log(numbers);
 
-// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// let numbers2 = numbers;
+let numberss = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let numbers2 = numberss;
 
-// numbers = [];
+numbers = [];
 
-// console.log(numbers);
+console.log(numberss);
 
-// console.log(numbers2);
+console.log(numbers2);
 
 // -----------------------------------------------------------------------------
 
 // @@@ COMBINING & SLICING ARRAYS  -> concat() , slice()
 
-// let num1 = [1, 2, 3, 4, 5];
-// let num2 = [6, 7, 8, 9, 10];
+let nums1 = [1, 2, 3, 4, 5];
+let nums2 = [6, 7, 8, 9, 10];
 
-// let num3 = num1.concat(num2);
+let num3 = num1.concat(num2);
 
-// console.log(num3);
+console.log(num3);
 
-// let num4 = num3.slice(0, 5);
+let num4 = num3.slice(0, 5);
 
-// console.log(num4);
+console.log(num4);
 
 // FULL SLICING -->  slice()
 
@@ -124,14 +124,14 @@
 
 // @ Spread Operator
 
-// let num1 = [1, 2, 3, 4, 5];
-// let num2 = [6, 7, 8, 9, 10];
+let num1 = [1, 2, 3, 4, 5];
+let num2 = [6, 7, 8, 9, 10];
 
-// let combined = [...num1, ...num2];
+let combinedd = [...num1, ...num2];
 
-// let combined = [...num1, "a", false, "b", ...num2];
+let combined = [...num1, "a", false, "b", ...num2];
 
-// console.log(combined);
+console.log(combined);
 
 // -----------------------------------------------------------------------------
 
@@ -139,79 +139,79 @@
 
 // for of
 
-// let arr = [10, 20, 30, 40, 50];
+let ar = [10, 20, 30, 40, 50];
 
-// for (let value of arr) {
-//   console.log(value);
-// }
+for (let value of arr) {
+  console.log(value);
+}
 
-// forEach
+forEach;
 
-// arr.forEach((number) => console.log(number));
+arr.forEach((number) => console.log(number));
 
 // -----------------------------------------------------------------------------
 
 // @ Joining ARRAY
 
-// let arr = [10, 20, 30, 40, 50];
+let a = [10, 20, 30, 40, 50];
 
-// const joined = arr.join(" ");
+const joinedd = arr.join(" ");
 
-// console.log(joined);
+console.log(joinedd);
 
 // -----------------------------------------------------------------------------
 
 // @ Split ARRAY
 
-// let message = "This is my first message";
-// let parts = message.split(" ");
+let message = "This is my first message";
+let parts = message.split(" ");
 
-// console.log(parts);
+console.log(parts);
 
-// let joined = parts.join(" ");
-// console.log(joined);
+let joined = parts.join(" ");
+console.log(joined);
 
 // -----------------------------------------------------------------------------
 
 // @ Sorting ARRAY
 
-// let arr = [40, 80, 10, 30, 20, 35, 50, 40];
+let arra = [40, 80, 10, 30, 20, 35, 50, 40];
 
-// let fin1 = arr.sort();
+let fin1 = arr.sort();
 
-// let fin = fin1.reverse();
+let fin = fin1.reverse();
 
-// console.log(fin);
+console.log(fin);
 
-// let arr = [
-//   { id: 1, name: "Pranav" },
-//   { id: 2, name: "Ashish" },
-// ];
+let arrr = [
+  { id: 1, name: "Pranav" },
+  { id: 2, name: "Ashish" },
+];
 
-// arr.sort((a1, a2) => a1.name > a2.name);
-// console.log(arr);
+arr.sort((a1, a2) => a1.name > a2.name);
+console.log(arr);
 
 // -----------------------------------------------------------------------------
 
 // @ filtering ARRAY .filter(callback)
 
-// let arr = [1, 2, -3, -5];
+let array = [1, 2, -3, -5];
 
-// let filtered = arr.filter((value) => value >= 0);
+let filteredd = arr.filter((value) => value >= 0);
 
-// console.log(filtered);
+console.log(filteredd);
 
 // -----------------------------------------------------------------------------
 
 // @  Mapping ARRAY *(.map)  => Map each element of array to something else
 
-// let arr = [7, 8, 9, 10];
+let arr = [7, 8, 9, 10];
 
-// console.log(arr);
+console.log(arr);
 
-// let items = arr.map((value) => "student_no " + value);
+let itemss = arr.map((value) => "student_no " + value);
 
-// console.log(items);
+console.log(itemss);
 
 // -----------------------------------------------------------------------------
 
@@ -219,7 +219,7 @@
 
 let number = [1, 2, -6, -9];
 
-// let filtered = number.filter((value) => value >= 0);
+let filtered = number.filter((value) => value >= 0);
 
 // let items = filtered.map(function (num) {
 //   return { value: num };
