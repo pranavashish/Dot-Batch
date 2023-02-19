97c1fd6c949167f5209dba2d60b06dcf52a6b37a
pranavashish <pranavashish@gmail.com>
2023-02-20 00:59:57 +0530

    Event Listeners Completed

--- DOM + Modern JS/Level_2/script.js
+++ DOM + Modern JS/Level_2/script.js
@@ -1,46 +1,46 @@
-// // document.addEventListener("click", function () {
-// //   console.log("I have clicked on the document");
-// // });
+document.addEventListener("click", function () {
+  console.log("I have clicked on the document");
+});
 
-// function eventFunction() {
-//   console.log("I have clicked on the document");
-// }
+function eventFunction() {
+  console.log("I have clicked on the document");
+}
 
-// document.addEventListener("click", eventFunction);
-// // document.removeEventListener("click", eventFunction);
+document.addEventListener("click", eventFunction);
+document.removeEventListener("click", eventFunction);
 
-// let content = document.querySelector("#wrapper");
-// content.addEventListener("click", function (event) {
-//   console.log(event);
-// });
+let content = document.querySelector("#wrapper");
+content.addEventListener("click", function (event) {
+  console.log(event);
+});
 
-// let links = document.querySelectorAll("a");
-// let third = links[2];
+let links = document.querySelectorAll("a");
+let third = links[2];
 
-// third.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   console.log("mazza aaya");
-// });
+third.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("mazza aaya");
+});
 
-// third.removeEventListener("click", function (event) {
-//   event.preventDefault();
-//   console.log("mazza aaya");
-// });
+third.removeEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("mazza aaya");
+});
 
-// let myDiv = document.createElement("div");
-// function paraStatus(event) {
-//   console.log("I have clicked on para" + event.target.textContent);
-// }
+let myDiv = document.createElement("div");
+function paraStatus(event) {
+  console.log("I have clicked on para" + event.target.textContent);
+}
 
-// myDiv.addEventListener("click", paraStatus);
-// for (let i = 1; i <= 100; i++) {
-//   let newElement = document.createElement("p");
-//   newElement.textContent = "This is para " + i;
+myDiv.addEventListener("click", paraStatus);
+for (let i = 1; i <= 100; i++) {
+  let newElement = document.createElement("p");
+  newElement.textContent = "This is para " + i;
 
-//   myDiv.appendChild(newElement);
-// }
+  myDiv.appendChild(newElement);
+}
 
-// document.body.appendChild(myDiv);
+document.body.appendChild(myDiv);
 
 let element = document.querySelector("#wrapper");
 
