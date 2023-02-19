5c326b7dd50724926604dba2a30d03b0c0d46bf5
pranavashish <pranavashish@gmail.com>
2023-02-20 00:58:55 +0530

    Event Listeners :3

--- DOM + Modern JS/Level_2/script.js
+++ DOM + Modern JS/Level_2/script.js
@@ -1,28 +1,51 @@
-// document.addEventListener("click", function () {
+// // document.addEventListener("click", function () {
+// //   console.log("I have clicked on the document");
+// // });
+
+// function eventFunction() {
 //   console.log("I have clicked on the document");
+// }
+
+// document.addEventListener("click", eventFunction);
+// // document.removeEventListener("click", eventFunction);
+
+// let content = document.querySelector("#wrapper");
+// content.addEventListener("click", function (event) {
+//   console.log(event);
 // });
 
-function eventFunction() {
-  console.log("I have clicked on the document");
-}
+// let links = document.querySelectorAll("a");
+// let third = links[2];
 
-document.addEventListener("click", eventFunction);
-// document.removeEventListener("click", eventFunction);
+// third.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   console.log("mazza aaya");
+// });
 
-let content = document.querySelector("#wrapper");
-content.addEventListener("click", function (event) {
-  console.log(event);
-});
+// third.removeEventListener("click", function (event) {
+//   event.preventDefault();
+//   console.log("mazza aaya");
+// });
 
-let links = document.querySelectorAll("a");
-let third = links[2];
+// let myDiv = document.createElement("div");
+// function paraStatus(event) {
+//   console.log("I have clicked on para" + event.target.textContent);
+// }
 
-third.addEventListener("click", function (event) {
-  event.preventDefault();
-  console.log("mazza aaya");
-});
+// myDiv.addEventListener("click", paraStatus);
+// for (let i = 1; i <= 100; i++) {
+//   let newElement = document.createElement("p");
+//   newElement.textContent = "This is para " + i;
 
-third.removeEventListener("click", function (event) {
-  event.preventDefault();
-  console.log("mazza aaya");
+//   myDiv.appendChild(newElement);
+// }
+
+// document.body.appendChild(myDiv);
+
+let element = document.querySelector("#wrapper");
+
+element.addEventListener("click", function (event) {
+  if (event.target.nodeName === "SPAN") {
+    console.log("span par click kiya hai" + event.target.textContent);
+  }
 });
