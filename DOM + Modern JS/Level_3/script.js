e69a1ccc0ca6615e3e2e2663c6828552a408d5bc
pranavashish <pranavashish@gmail.com>
2023-02-21 01:27:31 +0530

    Before event loop & call stack

--- DOM + Modern JS/Level_3/script.js
+++ DOM + Modern JS/Level_3/script.js
@@ -1,42 +1,47 @@
-// //adding 100 para
+//adding 100 para
 
-// const t1 = performance.now();
+const t1 = performance.now();
 
-// for (let i = 0; i <= 100; i++) {
-//   let newElement = document.createElement("p");
-//   newElement.textContent = "This is para " + i;
+for (let i = 0; i <= 100; i++) {
+  let newElement = document.createElement("p");
+  newElement.textContent = "This is para " + i;
 
-//   document.body.appendChild(newElement);
-// }
-// const t2 = performance.now();
-// console.log("This took: " + (t2 - t1) + " ms");
-// // optimizing a bit
+  document.body.appendChild(newElement);
+}
+const t2 = performance.now();
+console.log("This took: " + (t2 - t1) + " ms");
 
-// let myDiv = document.createElement("div");
-// const t3 = performance.now();
-// for (let i = 0; i <= 100; i++) {
-//   let element = document.createElement("p");
-//   element.textContent = "This is para " + i;
+// optimizing a bit
 
-//   myDiv.appendChild(element);
-// }
-// const t4 = performance.now();
+let myDiv = document.createElement("div");
+const t3 = performance.now();
+for (let i = 0; i <= 100; i++) {
+  let element = document.createElement("p");
+  element.textContent = "This is para " + i;
 
-// document.body.appendChild(myDiv);
-// console.log("This took: " + (t4 - t3) + " ms");
+  myDiv.appendChild(element);
+}
+const t4 = performance.now();
 
-// const t1 = performance.now();
-// let fragment = document.createDocumentFragment();
-// for (let i = 0; i <= 1000000; i++) {
-//   let newElements = document.createElement("p");
-//   newElements.textContent = "This is para " + i;
+document.body.appendChild(myDiv);
+console.log("This took: " + (t4 - t3) + " ms");
 
-//   fragment.appendChild(newElements);
-// }
+//refresh and repaint
 
-// document.body.appendChild(fragment);
-// const t2 = performance.now();
-// console.log("This took: " + (t2 - t1) + " ms");
+const to1 = performance.now();
+let fragment = document.createDocumentFragment();
+for (let i = 0; i <= 1000000; i++) {
+  let newElements = document.createElement("p");
+  newElements.textContent = "This is para " + i;
+
+  fragment.appendChild(newElements);
+}
+
+document.body.appendChild(fragment);
+const to2 = performance.now();
+console.log("This took: " + (to2 - to1) + " ms");
+
+//
 
 function addPara() {
   let para = document.createElement("p");
