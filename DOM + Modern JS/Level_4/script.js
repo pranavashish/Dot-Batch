243240ed072070f80f41fbe03fb4f8ce8fc468ce
pranavashish <pranavashish@gmail.com>
2023-02-25 15:57:00 +0530

    Counter App - Mini Project

--- DOM + Modern JS/Level_4/script.js
+++ DOM + Modern JS/Level_4/script.js
@@ -1,128 +1,128 @@
-// // Promise
+// Promise
 
-// let meraPromise1 = new Promise(function (resolve, reject) {
-//   setTimeout(function () {
-//     console.log("Im inside Promise 1");
-//   }, 5000);
-//   //   resolve(2000);
-//   //   reject(new Error("Error aa gaya bhai xD"));
-// });
+let meraPromise1 = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Im inside Promise 1");
+  }, 5000);
+  //   resolve(2000);
+  //   reject(new Error("Error aa gaya bhai xD"));
+});
 
-// let meraPromise2 = new Promise(function (resolve, reject) {
-//   setTimeout(function () {
-//     console.log("Im inside Promise 2");
-//   }, 3000);
-//   //   resolve(2000);
-//   //   reject(new Error("Error aa gaya bhai xD"));
-// });
+let meraPromise2 = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Im inside Promise 2");
+  }, 3000);
+  //   resolve(2000);
+  //   reject(new Error("Error aa gaya bhai xD"));
+});
 
-// let meraPromise = new Promise(function (resolve, reject) {
-//   setTimeout(function () {
-//     console.log("Im inside Promise");
-//   }, 2000);
-//   //resolve(2000);
-//   reject(new Error("Error aa gaya bhai xD"));
-// });
+let meraPromise = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Im inside Promise");
+  }, 2000);
+  //resolve(2000);
+  reject(new Error("Error aa gaya bhai xD"));
+});
 
-// meraPromise.then(
-//   (value) => {
-//     console.log(value);
-//   },
-//   (error) => {
-//     console.log("error aaaya broo");
-//   }
-// );
+meraPromise.then(
+  (value) => {
+    console.log(value);
+  },
+  (error) => {
+    console.log("error aaaya broo");
+  }
+);
 
-// // meraPromise.catch();
+// meraPromise.catch();
 
-// // console.log("pehla");
+// console.log("pehla");
 
-// let wada1 = new Promise(function (resolve, reject) {
-//   setTimeout(() => {
-//     console.log("setTimout 1 started");
-//   }, 2000);
-//   resolve(true);
-// });
+let wada1 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    console.log("setTimout 1 started");
+  }, 2000);
+  resolve(true);
+});
 
-// let output = wada1.then(() => {
-//   let wada2 = new Promise(function (resolve, reject) {
-//     setTimeout(() => {
-//       console.log("setTimout 1.5 started");
-//     }, 3000);
-//     resolve("Wada 2 to resolved");
-//   });
-//   return wada2;
-// });
-// output.then((value) => console.log(value));
+let output = wada1.then(() => {
+  let wada2 = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      console.log("setTimout 1.5 started");
+    }, 3000);
+    resolve("Wada 2 to resolved");
+  });
+  return wada2;
+});
+output.then((value) => console.log(value));
 
-// //  ASYNC AWAIT
+//  ASYNC AWAIT
 
-// async function abcd() {
-//   return 7;
-// }
+async function abcd() {
+  return 7;
+}
 
-// abcd();
+abcd();
 
-// async function utility() {
-//   let delhiWeather = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve("Delhi mein bohot garmi hai");
-//     }, 5000);
-//   });
+async function utility() {
+  let delhiWeather = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Delhi mein bohot garmi hai");
+    }, 5000);
+  });
 
-//   let mumbaiWeather = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve("Mumbai is cool");
-//     }, 7000);
-//   });
+  let mumbaiWeather = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Mumbai is cool");
+    }, 7000);
+  });
 
-//   let dM = await delhiWeather;
-//   let mM = await mumbaiWeather;
+  let dM = await delhiWeather;
+  let mM = await mumbaiWeather;
 
-//   return [dM, mM];
-// }
+  return [dM, mM];
+}
 
-// // @ Fetch API
+// @ Fetch API
 
-// // get
+// get
 
-// async function xyz() {
-//   let content = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-//   let output = await content.json();
-//   console.log(output);
-// }
+async function xyz() {
+  let content = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  let output = await content.json();
+  console.log(output);
+}
 
-// xyz();
+xyz();
 
-// // POST
+// POST
 
-// async function helper() {
-//   let options = {
-//     method: "POST",
-//     body: JSON.stringify({
-//       title: "Pranav",
-//       body: "Solid Body",
-//       userId: 2000,
-//     }),
-//     headers: {
-//       "Content-type": "application/json; charset=UTF-8",
-//     },
-//   };
+async function helper() {
+  let options = {
+    method: "POST",
+    body: JSON.stringify({
+      title: "Pranav",
+      body: "Solid Body",
+      userId: 2000,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  };
 
-//   let content = await fetch(
-//     "https://jsonplaceholder.typicode.com/posts",
-//     options
-//   );
-//   let response = content.json();
-//   return response;
-// }
+  let content = await fetch(
+    "https://jsonplaceholder.typicode.com/posts",
+    options
+  );
+  let response = content.json();
+  return response;
+}
 
-// async function utilities() {
-//   let ans = await helper();
-//   console.log(ans);
-// }
+async function utilities() {
+  let ans = await helper();
+  console.log(ans);
+}
 
-// utilities();
+utilities();
 
 // Closures
 
