6294a2b42c878bae26eee7f6672a7f7f348dfaa7
pranavashish <pranavashish@gmail.com>
2023-03-07 20:07:41 +0530

    Weather App HTML+JS done

--- JS Mini Projects 1/Weather App -  Project/main.js
+++ JS Mini Projects 1/Weather App -  Project/main.js
@@ -48,12 +48,11 @@ searchTab.addEventListener("click", () => {
   switchTab(searchTab);
 });
 
-//check if coordinates are already present in seesion storage
+//check if cordinates are already present in session storage
 function getfromSessionStorage() {
   const localCoordinates = sessionStorage.getItem("user-coordinates");
-
   if (!localCoordinates) {
-    // agar local coordinates nahi mile
+    //agar local coordinates nahi mile
     grantAccessContainer.classList.add("active");
   } else {
     const coordinates = JSON.parse(localCoordinates);
@@ -62,30 +61,30 @@ function getfromSessionStorage() {
 }
 
 async function fetchUserWeatherInfo(coordinates) {
-  const { lati, long } = coordinates;
-  //make grant container ivisible
+  const { lat, lon } = coordinates;
+  // make grantcontainer invisible
   grantAccessContainer.classList.remove("active");
-  // make loader visible
+  //make loader visible
   loadingScreen.classList.add("active");
 
   //API CALL
-
   try {
-    const res = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${long}&appid=${API_KEY}&units=metric`
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     );
-    const data = await res.json();
+    const data = await response.json();
 
     loadingScreen.classList.remove("active");
     userInfoContainer.classList.add("active");
     renderWeatherInfo(data);
   } catch (err) {
-    //HW
+    loadingScreen.classList.remove("active");
+    HW; //
   }
 }
 
 function renderWeatherInfo(weatherInfo) {
-  //firstly we have to fetch the element
+  //fistly, we have to fethc the elements
 
   const cityName = document.querySelector("[data-cityName]");
   const countryIcon = document.querySelector("[data-countryIcon]");
@@ -96,8 +95,9 @@ function renderWeatherInfo(weatherInfo) {
   const humidity = document.querySelector("[data-humidity]");
   const cloudiness = document.querySelector("[data-cloudiness]");
 
-  //fetch values from weatherInfo
+  console.log(weatherInfo);
 
+  //fetch values from weatherINfo object and put it UI elements
   cityName.innerText = weatherInfo?.name;
   countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
   desc.innerText = weatherInfo?.weather?.[0]?.description;
@@ -108,4 +108,51 @@ function renderWeatherInfo(weatherInfo) {
   cloudiness.innerText = `${weatherInfo?.clouds?.all}%`;
 }
 
-gran;
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    alert("No ge olocation support available");
+  }
+}
+
+function showPosition(position) {
+  const userCoordinates = {
+    lat: position.coords.latitude,
+    lon: position.coords.longitude,
+  };
+
+  sessionStorage.setItem("user-coordinates", JSON.stringify(userCoordinates));
+  fetchUserWeatherInfo(userCoordinates);
+}
+
+const grantAccessButton = document.querySelector("[data-grantAccess]");
+grantAccessButton.addEventListener("click", getLocation);
+
+const searchInput = document.querySelector("[data-searchInput]");
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let cityName = searchInput.value;
+
+  if (cityName === "") return;
+  else fetchSearchWeatherInfo(cityName);
+});
+
+async function fetchSearchWeatherInfo(city) {
+  loadingScreen.classList.add("active");
+  userInfoContainer.classList.remove("active");
+  grantAccessContainer.classList.remove("active");
+
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    );
+    const data = await response.json();
+    loadingScreen.classList.remove("active");
+    userInfoContainer.classList.add("active");
+    renderWeatherInfo(data);
+  } catch (err) {
+    //hW
+  }
+}
