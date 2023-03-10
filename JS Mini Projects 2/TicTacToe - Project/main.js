4e97db981a020928575bcf01c7d37647cdb3f9f2
pranavashish <pranavashish@gmail.com>
2023-03-11 01:17:48 +0530

    TicTacToe Completed

--- JS Mini Projects 2/TicTacToe - Project/main.js
+++ JS Mini Projects 2/TicTacToe - Project/main.js
@@ -20,8 +20,100 @@ const winningPositions = [
 function initGame() {
   currentPlayer = "X";
   gameGrid = ["", "", "", "", "", "", "", "", ""];
+  //UI pe empty karna padega na
+  boxes.forEach((box, index) => {
+    box.innerText = "";
+    boxes[index].style.pointerEvents = "all";
+    //initalize box with default css
+    box.classList = `box box${index + 1}`;
+  });
+
   newGameBtn.classList.remove("active");
   gameInfo.innerText = `Current Player - ${currentPlayer}`;
 }
 
 initGame();
+
+function checkGameOver() {
+  let answer = "";
+
+  winningPositions.forEach((position) => {
+    //all 3 boxes should be non empty anad exactly same in value
+    if (
+      (gameGrid[position[0]] !== "" ||
+        gameGrid[position[1]] !== "" ||
+        gameGrid[position[2]] !== "") &&
+      gameGrid[position[0]] === gameGrid[position[1]] &&
+      gameGrid[position[1]] === gameGrid[position[2]]
+    ) {
+      // check if winner is X
+      if (gameGrid[position[0]] === "X") {
+        answer = "X";
+      } else {
+        answer = "O";
+      }
+
+      //disable pointer
+
+      boxes.forEach((box) => {
+        box.style.pointerEvents = "none";
+      });
+
+      // green karo
+      boxes[position[0]].classList.add("win");
+      boxes[position[1]].classList.add("win");
+      boxes[position[2]].classList.add("win");
+    }
+  });
+  if (answer !== "") {
+    gameInfo.innerText = `Winner Player = ${answer}`;
+    newGameBtn.classList.add("active");
+    return;
+  }
+
+  //checking whether there is tie
+  let fillCount = 0;
+  gameGrid.forEach((box) => {
+    if (box !== "") {
+      fillCount++;
+    }
+  });
+
+  // filling board then there is tie
+
+  if (fillCount === 9) {
+    gameInfo.innerText = "Game Tied !";
+    newGameBtn.classList.add("active");
+  }
+}
+
+function swapTurn() {
+  if (currentPlayer === "X") {
+    currentPlayer = "O";
+  } else {
+    currentPlayer = "X";
+  }
+  //   UI Update
+
+  gameInfo.innerText = `Current Player - ${currentPlayer}`;
+}
+
+function handleClick(index) {
+  if (gameGrid[index] === "") {
+    boxes[index].innerText = currentPlayer;
+    gameGrid[index] = currentPlayer;
+    boxes[index].style.pointerEvents = "none";
+    //swap karo O X
+    swapTurn();
+    //check koi jeet gaya kya?
+    checkGameOver();
+  }
+}
+
+boxes.forEach((box, index) => {
+  box.addEventListener("click", () => {
+    handleClick(index);
+  });
+});
+
+newGameBtn.addEventListener("click", initGame);
