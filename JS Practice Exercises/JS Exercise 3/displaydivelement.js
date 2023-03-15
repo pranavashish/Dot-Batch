56c2ab5ffae15456da6415978e088b30214320d6
pranavashish <pranavashish@gmail.com>
2023-03-16 00:25:16 +0530

    JS Ex 3 half

---
+++ JS Practice Exercises/JS Exercise 3/displaydivelement.js
@@ -0,0 +1,8 @@
+function toggler(divID) {
+  const div = document.querySelector("#divID");
+  if (div.style.display === "none") {
+    div.style.display === "block";
+  } else {
+    div.style.display === "none";
+  }
+}
