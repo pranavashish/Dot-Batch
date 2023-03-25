f5566017b2d5eacce3b5bb23c439811a869ff40a
pranavashish <pranavashish@gmail.com>
2023-03-25 20:23:35 +0530

    Level_1 Completed

--- React Intermediate/Level_1/formapp/src/App.js
+++ React Intermediate/Level_1/formapp/src/App.js
@@ -18,8 +18,9 @@ function App() {
     email: "",
     comment: "",
     isVisible: true,
+    mode: "",
   });
-  console.log(formData);
+  // console.log(formData);
   function changeHandler(event) {
     const { name, value, checked, type } = event.target;
     setFormData((prevFormData) => {
@@ -30,9 +31,15 @@ function App() {
     });
   }
 
+  function sumbitHandler(event) {
+    event.preventDefault();
+    console.log("Finally printing");
+    console.log(formData);
+  }
+
   return (
     <div className="App">
-      <form>
+      <form onSubmit={sumbitHandler}>
         <br></br>
         <input
           type="text"
@@ -77,6 +84,51 @@ function App() {
           checked={formData.isVisible}
         ></input>
         <label htmlFor="isVisible">Am I Visible?</label>
+        <br></br>
+        <br></br>
+        <fieldset>
+          <legend>Mode</legend>
+          <input
+            type="radio"
+            onChange={changeHandler}
+            name="mode"
+            value="Online-Mode"
+            id="Online-Mode"
+            checked={formData.mode === "Online-Mode"}
+          ></input>
+          <label htmlFor="Online-Mode">Online Mode</label>
+          <input
+            type="radio"
+            onChange={changeHandler}
+            name="mode"
+            value="Offline-Mode"
+            id="Offline-Mode"
+            checked={formData.mode === "Offline-Mode"}
+          ></input>
+          <label htmlFor="Offline-Mode">Offline Mode</label>
+        </fieldset>
+        <label>Tell me about yor car </label>
+        <br></br>
+        <select
+          onChange={changeHandler}
+          name="favCar"
+          id="favCar"
+          value={formData.favCar}
+        >
+          <option value="scorpio">Scorpio</option>
+          <option value="Audi">Audi R8</option>
+          <option value="BMW">BMW X7</option>
+          <option value="G-Wagon">G-Wagon</option>
+          <option value="Wagon-R">Wagon-R</option>
+          <option value="Thar">Thar</option>
+          <option value="Alto">Alto</option>
+        </select>
+        <br></br>
+        <br></br>
+        {/* <input type="submit" value="submit">
+          Submit
+        </input> */}
+        <button>Submit</button>
       </form>
     </div>
   );
